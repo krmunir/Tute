9aa1f3aedac6ef3d87abf1c4cf32473828ba6aae
Kash Admin <krmunir@gmail.com>
2018-07-07 10:46:29 +0930

    4.7 Q1 structs

--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include<string>
 
+struct advertising {
+	int noOfAdsShown = 0;
+	int perOfAdsClicked = 0;
+	float earningsPerClick = 0.0;
+};
+
+void showAdDetails(advertising ad);
+
 int main() {
 
-	enum class MonsterType
-	{
-		//orcs, goblins, trolls, ogres, and skeletons
-		ORC,
-		GOBLIN,
-		TROLL,
-		OGRE,
-		SKELETON
-	};
+	
+	advertising adType1{ 20,40,.05 };
 
-	MonsterType monster1{ MonsterType::GOBLIN };
-	MonsterType monster2{ MonsterType::OGRE };
+	showAdDetails(adType1);
 
-	std::cout << "Monster 1 is of type " << static_cast<int>(monster1) << std::endl;
-	std::cout << "Monster 2 is of type " << static_cast<int>(monster2) << std::endl;
 
-	//std::cin.ignore(32767, '\n');
-	std::cin.get();
 
 	return 0;
+}
+
+void showAdDetails(advertising ad) {
+	std::cout << "The number of ads shown = " << ad.noOfAdsShown << std::endl;
+	std::cout << "The percentage of ads clicked = " << ad.perOfAdsClicked << std::endl;
+	std::cout << "The earning per ad = " << ad.earningsPerClick << std::endl;
+	std::cout << "Total earnings = " << ad.noOfAdsShown*ad.perOfAdsClicked/100*ad.earningsPerClick << std::endl;
+	std::cin.get();
 }

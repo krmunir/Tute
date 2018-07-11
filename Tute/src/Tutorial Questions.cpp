f7b03e6cb62467e6dc960bc264fc9445ab7f6b10
Kash Admin <krmunir@gmail.com>
2018-07-11 17:54:00 +0930

    sec 5 q3 bubble sort complete!

--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -1,22 +1,65 @@
+#include <algorithm> // for std::swap, use <utility> instead if C++11
 #include <iostream>
-#include <string>
+
+void printArray( int(&array)[9]);
 
 int main()
 {
-	int scores[] = { 84, 92, 76, 81, 56 };
-	const int numStudents = sizeof(scores) / sizeof(scores[0]);
+	const int length(9);
+	int array[length] = { 6, 3, 2, 9, 7, 1, 5, 4, 8 };
+	char sortLocations[length] = {};
 
-	int maxIndex{}; // keep track of index of largest score
+	//print unsorted array
+	std::cout << "Unsorted array:" << std::endl;
+	printArray(array);
 
-					  // now look for a larger score
-	for (int student = 0; student < numStudents; ++student)
-		if (scores[student] > scores[maxIndex])
-			maxIndex = student;
+	bool isSorted{};
+	int arrayIndex{ 0 };
 
-	std::cout << "The best score was " << scores[maxIndex] << '\n';
+	do {
+		arrayIndex = 0;
+		isSorted = true; //assume array is sorted, if after a run through all elements any swaps are required set to false
+		for (int &val : array) {
+			if (array[arrayIndex + 1] < array[arrayIndex]) {
+				std::swap(array[arrayIndex + 1], array[arrayIndex]);
+				isSorted = false;
+				
+				//print sort locations
+				//clear old locations
+				for (char &a : sortLocations) {
+					a = ' ';
+				}
 
-	std::cin.ignore(32767, '\n');
+				//set swap locations
+				sortLocations[arrayIndex] = 'X';
+				sortLocations[arrayIndex+1] = 'X';
+
+				//print sort locations
+				for (char &a : sortLocations)
+					std::cout << a << ' ';
+				std::cout << std::endl;
+
+				//print array
+				printArray(array);
+			}
+			++arrayIndex;
+			if (arrayIndex == 8)
+				break; //highest array index is 8
+		}
+
+	} while (!isSorted);
+
+	// Now that the whole array is sorted, print our sorted array as proof it works
+	std::cout << "Sorted array:" << std::endl;
+	printArray(array);
+
 	std::cin.get();
 
 	return 0;
+}
+
+void printArray( int(&array)[9]) {
+	for (int &element : array)
+		std::cout << element << " ";
+	std::cout << std::endl << std::endl;
 }

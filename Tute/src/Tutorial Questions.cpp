5bc898ac6003b6416ab0b53c81a9f17cdb441a39
Kash Admin <krmunir@gmail.com>
2018-07-11 15:26:56 +0930

    sec 5 q3

--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -3,50 +3,18 @@
 
 int main()
 {
-	int array[] { 4, 6, 7, 3, 8, 2, 1, 9, 5 };
-	int searchVal {};
+	int scores[] = { 84, 92, 76, 81, 56 };
+	const int numStudents = sizeof(scores) / sizeof(scores[0]);
 
-	bool isInvalidEntry = false;
-	do
-	{
-		isInvalidEntry = false;
-		std::cout << "Enter an integer between 1 and 9: ";
-		std::cin >> searchVal;
+	int maxIndex{}; // keep track of index of largest score
 
-		// if the user entered something invalid
-		if (std::cin.fail())
-		{
-			isInvalidEntry = true;
-			std::cin.clear(); // reset any error flags
-			std::cin.ignore(32767, '\n'); // ignore any characters in the input buffer
-		}
+					  // now look for a larger score
+	for (int student = 0; student < numStudents; ++student)
+		if (scores[student] > scores[maxIndex])
+			maxIndex = student;
 
-		//also invalid if not between 1 and 9
-		else if ((searchVal < 1) || (searchVal>9))
-			isInvalidEntry = true;
+	std::cout << "The best score was " << scores[maxIndex] << '\n';
 
-	} while (isInvalidEntry);
-
-	//print whole array
-	for (int i = 0; i < 9; ++i)
-	{
-		std::cout << i << " ";
-	}
-	std::cout << std::endl;
-	for (const int &val : array)
-		std::cout << val << " ";
-
-	//find search value 
-	int arrayIndex{ 0 };
-	for (const int &val : array) {
-		if (val == searchVal) {
-			std::cout << std::endl<<searchVal << " found at array index " << arrayIndex;
-			break;
-		}
-		else ++arrayIndex;
-	}
-
-	//pause output window
 	std::cin.ignore(32767, '\n');
 	std::cin.get();
 

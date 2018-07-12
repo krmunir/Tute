ad65a55e30b85e280531ba7d9d80295523890801
Kash Admin <krmunir@gmail.com>
2018-07-12 18:34:31 +0930

    sec 5

--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -1,65 +1,33 @@
 #include <algorithm> // for std::swap, use <utility> instead if C++11
 #include <iostream>
+#include <string>
 
-void printArray( int(&array)[9]);
 
 int main()
 {
-	const int length(9);
-	int array[length] = { 6, 3, 2, 9, 7, 1, 5, 4, 8 };
-	char sortLocations[length] = {};
+	std::cout << "How many names do you wish to enter?" << std::endl;
+	int noOfNames{};
+	std::cin >> noOfNames;
 
-	//print unsorted array
-	std::cout << "Unsorted array:" << std::endl;
-	printArray(array);
+	std::string *names = new std::string[noOfNames];
 
-	bool isSorted{};
-	int arrayIndex{ 0 };
+	for (int loop = 0; loop < noOfNames; ++loop)
+	{
+		std::cout << "Enter name #" << (loop + 1) << ": ";
+		std::getline(std::cin, names[loop]);
+	}
 
-	do {
-		arrayIndex = 0;
-		isSorted = true; //assume array is sorted, if after a run through all elements any swaps are required set to false
-		for (int &val : array) {
-			if (array[arrayIndex + 1] < array[arrayIndex]) {
-				std::swap(array[arrayIndex + 1], array[arrayIndex]);
-				isSorted = false;
-				
-				//print sort locations
-				//clear old locations
-				for (char &a : sortLocations) {
-					a = ' ';
-				}
+	//print names
+	for (int i = 0; i < noOfNames; ++i)
+	{
+		std::cout << "Name #" << i << " is " << names[i] << std::endl;
+	}
 
-				//set swap locations
-				sortLocations[arrayIndex] = 'X';
-				sortLocations[arrayIndex+1] = 'X';
 
-				//print sort locations
-				for (char &a : sortLocations)
-					std::cout << a << ' ';
-				std::cout << std::endl;
-
-				//print array
-				printArray(array);
-			}
-			++arrayIndex;
-			if (arrayIndex == 8)
-				break; //highest array index is 8
-		}
-
-	} while (!isSorted);
-
-	// Now that the whole array is sorted, print our sorted array as proof it works
-	std::cout << "Sorted array:" << std::endl;
-	printArray(array);
-
+	std::cin.ignore(32767, '\n');
 	std::cin.get();
 
 	return 0;
 }
 
-void printArray( int(&array)[9]) {
-	for (int &element : array)
-		std::cout << element << " ";
-	std::cout << std::endl << std::endl;
-}
+		

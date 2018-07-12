419a18c71cb3840240c69c3ff9d1dfadd45185d3
Kash Admin <krmunir@gmail.com>
2018-07-12 18:49:56 +0930

    sec 6 q1

--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -2,27 +2,22 @@
 #include <iostream>
 #include <string>
 
+namespace item {
+	enum item {
+		HEALTH_POTIONS,
+		TORCHES,
+		ARROWS
+	};
+
+}
+
+int countTotalItems(int *itemsArray);
 
 int main()
 {
-	std::cout << "How many names do you wish to enter?" << std::endl;
-	int noOfNames{};
-	std::cin >> noOfNames;
-
-	std::string *names = new std::string[noOfNames];
-
-	for (int loop = 0; loop < noOfNames; ++loop)
-	{
-		std::cout << "Enter name #" << (loop + 1) << ": ";
-		std::getline(std::cin, names[loop]);
-	}
-
-	//print names
-	for (int i = 0; i < noOfNames; ++i)
-	{
-		std::cout << "Name #" << i << " is " << names[i] << std::endl;
-	}
-
+	int    itemsCarried[3]{ 2,5,10 };
+	
+	std::cout << "No of items carried = " << countTotalItems(itemsCarried);
 
 	std::cin.ignore(32767, '\n');
 	std::cin.get();
@@ -30,4 +25,11 @@ int main()
 	return 0;
 }
 
-		
+int countTotalItems(int *itemsArray) {
+	int totalItems{};
+	for (int i = 0; i < 3; ++i)
+	{
+		totalItems += itemsArray[i];
+	}
+	return totalItems;
+}

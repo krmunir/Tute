e8bd9f21619ef5154243bc189a9f7ada01909307
Kash Admin <krmunir@gmail.com>
2018-07-18 13:51:17 +0930

    7 q1

--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -5,18 +5,17 @@
 #include <cstdlib>
 #include <ctime>
 
-int factorial(int num);
+double max(const double a, const double b);
+
+void swap(int &a, int &b);
+
+int& getLargestElement(int *array, int length);
 
 int main()
 {
-	std::cout << "3 factorial = " << factorial(3) << std::endl;
-	std::cout << "Should be " << (3 * 2) << std::endl << std::endl;
 
-	std::cout << "5 factorial = " << factorial(5) << std::endl;
-	std::cout << "Should be " << (5*4*3 * 2) << std::endl << std::endl;
 
-	std::cout << "7 factorial = " << factorial(7) << std::endl;
-	std::cout << "Should be " << (7*6*5*4*3 * 2) << std::endl << std::endl;
+
 
 
 	std::cin.ignore(32767, '\n');
@@ -25,14 +24,4 @@ int main()
 	return 0;
 }
 
-int factorial(int num)
-{
 
-	if (num > 1)
-		return (num*factorial(num - 1));
-	
-	else 
-		return 1;
-
-	
-}

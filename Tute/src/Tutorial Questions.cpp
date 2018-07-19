8fcb01347465e6bd5cce5f6b6f536adc59e1f864
Kash Admin <krmunir@gmail.com>
2018-07-19 12:50:39 +0930

    8.3 q2b

--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -24,14 +24,34 @@ int main()
 			std::cout << "<" << m_x<< ", " << m_y << ", " << m_z << ">" << std::endl;
 		}
 
+		bool isEqual(Point3d p) {
+			if ((p.m_x == m_x) && (p.m_y == m_y) && (p.m_z == m_z))
+				return true;
+			else
+				return false;
+		}
 	};
 
 	
 
-	Point3d point;
-	point.setValues(1.0, 2.0, 3.0);
+	Point3d point1;
+	point1.setValues(1.0, 2.0, 3.0);
 
-	point.print();
+	Point3d point2;
+	point2.setValues(1.0, 2.0, 3.0);
+
+	if (point1.isEqual(point2))
+		std::cout << "point1 and point2 are equal\n";
+	else
+		std::cout << "point1 and point2 are not equal\n";
+
+	Point3d point3;
+	point3.setValues(3.0, 4.0, 5.0);
+
+	if (point1.isEqual(point3))
+		std::cout << "point1 and point3 are equal\n";
+	else
+		std::cout << "point1 and point3 are not equal\n";
 
 	//std::cin.ignore(32767, '\n');
 	std::cin.get();

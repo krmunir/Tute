2389647180517b201150c524b04778deac43aaaa
Kash Admin <krmunir@gmail.com>
2018-07-24 12:43:07 +0930

    9.1 q1c

---
+++ Tute/Fraction.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+//Add overloaded multiplication operators to handle multiplication between a Fraction and integer, and between two Fractions. 
+//Use the friend function method.
+
+class Fraction
+{
+private:
+	int m_num;
+	int m_den;
+public:
+	Fraction(int num=0, int den=1) : m_num{num}, m_den{den} {}
+
+	void print() {
+		std::cout << "Fraction = " << m_num << " / " << m_den << std::endl;
+	}
+	friend Fraction operator*(Fraction f, int i);
+	friend Fraction operator*(Fraction f1, Fraction f2);
+private:
+	int gcd(int a, int b) {
+		return (b == 0) ? (a > 0 ? a : -a) : gcd(b, a % b);
+	}
+};
+
--- Tute/src/Tutorial Questions.cpp
+++ Tute/src/Tutorial Questions.cpp
@@ -6,31 +6,32 @@
 #include <ctime>
 #include <cassert>
 
+#include "..\Fraction.h"
+
+Fraction operator*(Fraction f, int i);
+Fraction operator*(Fraction f1, Fraction f2);
+
 
 int main()
 {
-	class RGBA {
-	private:
-		std::uint8_t m_red; 
-		std::uint8_t m_green;
-		std::uint8_t m_blue;
-		std::uint8_t m_alpha;
+	
+	Fraction f1(2, 5);
+	f1.print();
 
-	public:
-		RGBA(std::uint8_t r=0, std::uint8_t g=0, std::uint8_t b=0, std::uint8_t a=255)
-			: m_red{ r },
-			m_green{ g },
-			m_blue{ b },
-			m_alpha{ a }
-		{}
+	Fraction f2(3, 8);
+	f2.print();
 
-		void print() {
-			std::cout << "r=" << static_cast<int> (m_red) << ", g=" << static_cast<int> (m_green) << ", b=" << static_cast<int> (m_blue) << ", a=" << static_cast<int> (m_alpha);
-		}
-	};
+	Fraction f3 = f1 * f2;
+	f3.print();
 
-	RGBA teal(0, 127, 127);
-	teal.print();
+	Fraction f4 = f1 * 2;
+	f4.print();
+
+	Fraction f5 = 2 * f2;
+	f5.print();
+
+	Fraction f6 = Fraction(1, 2) * Fraction(2, 3) * Fraction(3, 4);
+	f6.print();
 
 
 	//std::cin.ignore(32767, '\n');
@@ -40,3 +41,25 @@ int main()
 }
 
 
+Fraction operator*(Fraction f, int i) {
+	Fraction result;
+	result.m_num = f.m_num*i;
+	result.m_den = f.m_den;
+	int GCD = result.gcd(result.m_num, result.m_den);
+	result.m_num /= GCD;
+	result.m_den /= GCD;
+
+	return result;
+}
+
+Fraction operator*(Fraction f1, Fraction f2) {
+	Fraction result;
+	result.m_num = f1.m_num*f2.m_num;
+	result.m_den = f1.m_den*f2.m_den;
+	int GCD = result.gcd(result.m_num, result.m_den);
+	result.m_num /= GCD;
+	result.m_den /= GCD;
+
+	return result;
+
+}
